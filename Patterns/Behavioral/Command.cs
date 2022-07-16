2e662d0e83f50e8cf6ba7473b570e53ba61b83e2
Yarre <yaroslav.syvukha@nure.ua>
2022-07-16 16:29:53 +0300

    Added all creator patterns

---
+++ Patterns/Behavioral/Command.cs
@@ -0,0 +1,123 @@
+﻿namespace Patterns.Behavioral
+{
+    // Интерфейс Команды объявляет метод для выполнения команд.
+    public interface ICommand
+    {
+        void Execute();
+    }
+
+    // Некоторые команды способны выполнять простые операции самостоятельно.
+    class SimpleCommand : ICommand
+    {
+        private string _payload = string.Empty;
+
+        public SimpleCommand(string payload)
+        {
+            _payload = payload;
+        }
+
+        public void Execute()
+        {
+            Console.WriteLine($"SimpleCommand: See, I can do simple things like printing ({_payload})");
+        }
+    }
+
+    // Но есть и команды, которые делегируют более сложные операции другим
+    // объектам, называемым «получателями».
+    class ComplexCommand : ICommand
+    {
+        private Receiver _receiver;
+
+        // Данные о контексте, необходимые для запуска методов получателя.
+        private string _a;
+
+        private string _b;
+
+        // Сложные команды могут принимать один или несколько объектов-
+        // получателей вместе с любыми данными о контексте через конструктор.
+        public ComplexCommand(Receiver receiver, string a, string b)
+        {
+            _receiver = receiver;
+            _a = a;
+            _b = b;
+        }
+
+        // Команды могут делегировать выполнение любым методам получателя.
+        public void Execute()
+        {
+            Console.WriteLine("ComplexCommand: Complex stuff should be done by a receiver object.");
+            _receiver.DoSomething(_a);
+            _receiver.DoSomethingElse(_b);
+        }
+    }
+
+    // Классы Получателей содержат некую важную бизнес-логику. Они умеют
+    // выполнять все виды операций, связанных с выполнением запроса. Фактически,
+    // любой класс может выступать Получателем.
+    class Receiver
+    {
+        public void DoSomething(string a)
+        {
+            Console.WriteLine($"Receiver: Working on ({a}.)");
+        }
+
+        public void DoSomethingElse(string b)
+        {
+            Console.WriteLine($"Receiver: Also working on ({b}.)");
+        }
+    }
+
+    // Отправитель связан с одной или несколькими командами. Он отправляет
+    // запрос команде.
+    class Invoker
+    {
+        private ICommand _onStart;
+
+        private ICommand _onFinish;
+
+        // Инициализация команд
+        public void SetOnStart(ICommand command)
+        {
+            _onStart = command;
+        }
+
+        public void SetOnFinish(ICommand command)
+        {
+            _onFinish = command;
+        }
+
+        // Отправитель не зависит от классов конкретных команд и получателей.
+        // Отправитель передаёт запрос получателю косвенно, выполняя команду.
+        public void DoSomethingImportant()
+        {
+            Console.WriteLine("Invoker: Does anybody want something done before I begin?");
+            if (_onStart is ICommand)
+            {
+                _onStart.Execute();
+            }
+
+            Console.WriteLine("Invoker: ...doing something really important...");
+
+            Console.WriteLine("Invoker: Does anybody want something done after I finish?");
+            if (_onFinish is ICommand)
+            {
+                _onFinish.Execute();
+            }
+        }
+    }
+
+    class Command
+    {
+        public static void Main()
+        {
+            // Клиентский код может параметризовать отправителя любыми
+            // командами.
+            Invoker invoker = new Invoker();
+            invoker.SetOnStart(new SimpleCommand("Say Hi!"));
+            Receiver receiver = new Receiver();
+            invoker.SetOnFinish(new ComplexCommand(receiver, "Send email", "Save report"));
+
+            invoker.DoSomethingImportant();
+        }
+    }
+}
---
+++ Patterns/Creators/Singleton.cs
@@ -0,0 +1,61 @@
+﻿namespace Singleton
+{
+    // Класс Одиночка предоставляет метод `GetInstance`, который ведёт себя как
+    // альтернативный конструктор и позволяет клиентам получать один и тот же
+    // экземпляр класса при каждом вызове.
+
+    // EN : The Singleton should always be a 'sealed' class to prevent class
+    // inheritance through external classes and also through nested classes.
+    public sealed class Singleton_
+    {
+        // Конструктор Одиночки всегда должен быть скрытым, чтобы предотвратить
+        // создание объекта через оператор new.
+        private Singleton_() { }
+
+        // Объект одиночки храниться в статичном поле класса. Существует
+        // несколько способов инициализировать это поле, и все они имеют разные
+        // достоинства и недостатки. В этом примере мы рассмотрим простейший из
+        // них, недостатком которого является полная неспособность правильно
+        // работать в многопоточной среде.
+        private static Singleton_ _instance;
+
+        // Это статический метод, управляющий доступом к экземпляру одиночки.
+        // При первом запуске, он создаёт экземпляр одиночки и помещает его в
+        // статическое поле. При последующих запусках, он возвращает клиенту
+        // объект, хранящийся в статическом поле.
+        public static Singleton_ GetInstance()
+        {
+            if (_instance == null)
+            {
+                _instance = new Singleton_();
+            }
+            return _instance;
+        }
+
+        // Наконец, любой одиночка должен содержать некоторую бизнес-логику,
+        // которая может быть выполнена на его экземпляре.
+        public void someBusinessLogic()
+        {
+            // ...
+        }
+    }
+
+    class Singleton
+    {
+        public static void Main(string[] args)
+        {
+            // Клиентский код.
+            Singleton_ s1 = Singleton_.GetInstance();
+            Singleton_ s2 = Singleton_.GetInstance();
+
+            if (s1 == s2)
+            {
+                Console.WriteLine("Singleton works, both variables contain the same instance.");
+            }
+            else
+            {
+                Console.WriteLine("Singleton failed, variables contain different instances.");
+            }
+        }
+    }
+}
